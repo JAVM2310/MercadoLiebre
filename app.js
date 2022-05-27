cdfa67fa3bda382a1e8052bb9a11ad0074e6adb7
JAVM2310 <juan.vazquez1611@gmail.com>
2022-05-26 21:02:36 -0300

    resuelta la ejercitacion de la clase 15

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/index.html"));
 });
 
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+
 app.listen(3000, () => {console.log("levantado el servidor en el puerto 3000")
 });
 
